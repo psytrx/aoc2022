428aa4660cd70d4f2c01bad50b3817b7d1fd15e9
fdumont <fj.dumont@gmail.com>
2022-12-05 22:08:11 +0100

    Solve day 4, part 1

--- Program.fs
+++ Program.fs
@@ -1,11 +1,11 @@
 ﻿let sw = new System.Diagnostics.Stopwatch()
 
-let filename = "./input/day03.txt"
+let filename = "./input/day04.txt"
 
 sw.Restart()
-let s1 = Day03.Solve1 filename
+let s1 = Day04.Solve1 filename
 printfn "%i (%O)" s1 sw.Elapsed
 
 sw.Restart()
-let s2 = Day03.Solve2 filename
+let s2 = Day04.Solve2 filename
 printfn "%i (%O)" s2 sw.Elapsed
---
+++ problems/Day04.fs
@@ -0,0 +1,27 @@
+module Day04
+
+let ParseRange (s: string) =
+    match s.Split("-") with
+    | [| l; u |] ->
+        let lower = System.Convert.ToInt32 l
+        let upper = System.Convert.ToInt32 u
+        (lower, upper)
+    | _ -> failwith "invalid range format"
+
+let ParsePair (s: string) =
+    match s.Split(",") with
+    | [| l; r |] -> (ParseRange l, ParseRange r)
+    | _ -> failwith "invalid pair format"
+
+let Contains inside outside =
+    match (inside, outside) with
+    | ((la, ua), (lb, ub)) -> la >= lb && la <= ub && ua >= lb && ua <= ub
+
+
+let Solve1 =
+    System.IO.File.ReadAllLines
+    >> Array.map ParsePair
+    >> Array.filter (fun (a, b) -> (Contains a b) || (Contains b a))
+    >> Array.length
+
+let Solve2 = System.IO.File.ReadAllLines >> Array.length
