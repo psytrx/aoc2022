4101e5f3374db7a66cd7ba968c930330d6a7b95d
fdumont <fj.dumont@gmail.com>
2022-12-07 16:04:02 +0100

    Prepare day 7

--- Program.fs
+++ Program.fs
@@ -1,11 +1,11 @@
 ﻿let sw = new System.Diagnostics.Stopwatch()
 
-let filename = "./input/day06.txt"
+let filename = "./input/day07-test.txt"
 
 sw.Restart()
-let s1 = Day06.solve1 filename
+let s1 = Day07.solve1 filename
 printfn "%O (%O)" s1 sw.Elapsed
 
 sw.Restart()
-let s2 = Day06.solve2 filename
+let s2 = Day07.solve2 filename
 printfn "%O (%O)" s2 sw.Elapsed
---
+++ problems/Day07.fs
@@ -0,0 +1,4 @@
+module Day07
+
+let solve1 filename = -1
+let solve2 filename = -1
