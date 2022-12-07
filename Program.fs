864de75792c0142d31e0b6458184ec63a8c5436e
fdumont <fj.dumont@gmail.com>
2022-12-07 09:33:02 +0100

    Add placeholders for day 5

--- Program.fs
+++ Program.fs
@@ -1,11 +1,11 @@
 ﻿let sw = new System.Diagnostics.Stopwatch()
 
-let filename = "./input/day04.txt"
+let filename = "./input/day05-test.txt"
 
 sw.Restart()
-let s1 = Day04.Solve1 filename
+let s1 = Day05.Solve1 filename
 printfn "%i (%O)" s1 sw.Elapsed
 
 sw.Restart()
-let s2 = Day04.Solve2 filename
+let s2 = Day05.Solve2 filename
 printfn "%i (%O)" s2 sw.Elapsed
---
+++ problems/Day05.fs
@@ -0,0 +1,7 @@
+module Day05
+
+let Solve1 () = 
+    -1
+
+let Solve2 () =
+    -1
