b8321256db99eb758833b6ef7fe86f9b184f5668
fdumont <fj.dumont@gmail.com>
2022-12-07 14:37:34 +0100

    Solve day 6, part 2

--- problems/Day06.fs
+++ problems/Day06.fs
@@ -1,22 +1,20 @@
 module Day06
 
 let findStartOfPacketMarker signal =
-    let rec aux i signal =
-        match signal with
-        | a :: b :: c :: d :: rest ->
-            let set = Set([ a; b; c; d ])
+    let rec aux i len signal =
+        let set = Set.ofList (List.take len signal)
 
-            if set.Count = 4 then
-                i + 4
-            else
-                aux (i + 1) (b :: c :: d :: rest)
-        | _ -> failwith "could not find start of packet marker"
+        if set.Count = len then
+            i + len
+        else
+            aux (i + 1) len (List.skip 1 signal)
 
     aux 0 signal
 
-let solve1 filename =
-    System.IO.File.ReadAllText(filename)
-    |> List.ofSeq
-    |> findStartOfPacketMarker
+let solve n =
+    System.IO.File.ReadAllText
+    >> List.ofSeq
+    >> findStartOfPacketMarker n
 
-let solve2 filename = -1
+let solve1 = solve 4
+let solve2 = solve 14
