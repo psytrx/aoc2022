f5161c90d3e99e570da94c5253d8e3c05bdd52ae
fdumont <fj.dumont@gmail.com>
2022-12-07 14:33:56 +0100

    Solve day 6, part 1

--- Program.fs
+++ Program.fs
@@ -1,6 +1,6 @@
 ﻿let sw = new System.Diagnostics.Stopwatch()
 
-let filename = "./input/day06-test.txt"
+let filename = "./input/day06.txt"
 
 sw.Restart()
 let s1 = Day06.solve1 filename
--- problems/Day06.fs
+++ problems/Day06.fs
@@ -1,4 +1,22 @@
 module Day06
 
-let solve1 filename = -1
+let findStartOfPacketMarker signal =
+    let rec aux i signal =
+        match signal with
+        | a :: b :: c :: d :: rest ->
+            let set = Set([ a; b; c; d ])
+
+            if set.Count = 4 then
+                i + 4
+            else
+                aux (i + 1) (b :: c :: d :: rest)
+        | _ -> failwith "could not find start of packet marker"
+
+    aux 0 signal
+
+let solve1 filename =
+    System.IO.File.ReadAllText(filename)
+    |> List.ofSeq
+    |> findStartOfPacketMarker
+
 let solve2 filename = -1
