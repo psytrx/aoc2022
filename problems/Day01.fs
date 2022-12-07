3ef50389b2df8e72988cf900d49b6a7593a80923
fdumont <fj.dumont@gmail.com>
2022-12-07 15:30:14 +0100

    Refactor day 1

--- problems/Day01.fs
+++ problems/Day01.fs
@@ -1,29 +1,14 @@
 module Day01
 
-let parseInput lines =
-    seq {
-        let mutable bag = []
-
-        for line in lines do
-            if line = "" then
-                yield bag
-                bag <- []
-            else
-                let c = System.Convert.ToInt32(line)
-                bag <- c :: bag
-
-        yield bag
-    }
-
-let loadBags =
+let parseInput =
     System.IO.File.ReadAllLines
-    >> parseInput
-    >> (Seq.map List.sum)
+    >> Util.blockWise " "
+    >> Seq.map (fun items -> items |> List.sumBy System.Convert.ToInt32)
 
-let solve1 = loadBags >> Seq.max
+let solve1 = parseInput >> Seq.max
 
 let solve2 =
-    loadBags
+    parseInput
     >> Seq.sortDescending
     >> Seq.take 3
     >> Seq.sum
