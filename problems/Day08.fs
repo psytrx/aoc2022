dbe88c24408ebc61f1a4293719f0a889393b1328
fdumont <fj.dumont@gmail.com>
2022-12-08 10:31:18 +0100

    Solve day 8, part 2

--- problems/Day08.fs
+++ problems/Day08.fs
@@ -9,11 +9,28 @@ let loadTreeGrid =
 let hasObstruction height xs = Array.exists (fun h -> h >= height) xs
 
 let getDirections y x (grid: 'a array array) =
-    let top = grid.[.. y - 1] |> Array.map (fun row -> row.[x])
-    let bottom = grid.[y + 1 ..] |> Array.map (fun row -> row.[x])
-    let left = grid.[y].[.. x - 1]
+    let up =
+        grid.[.. y - 1]
+        |> Array.map (fun row -> row.[x])
+        |> Array.rev
+
+    let down = grid.[y + 1 ..] |> Array.map (fun row -> row.[x])
+    let left = grid.[y].[.. x - 1] |> Array.rev
     let right = grid.[y].[x + 1 ..]
-    [ top; bottom; left; right ]
+
+    [ up; left; down; right ]
+
+let viewingDistance height row =
+    let rec aux height row =
+        match row with
+        | [] -> 0
+        | t :: rest ->
+            if t >= height then
+                1
+            else
+                1 + aux height rest
+
+    aux height row
 
 let isVisible y x (grid: int array array) =
     let height = grid.[y].[x]
@@ -22,6 +39,14 @@ let isVisible y x (grid: int array array) =
     |> getDirections y x
     |> List.exists (hasObstruction height >> not)
 
+let scenicScore y x (grid: int array array) =
+    let height = grid.[y].[x]
+
+    grid
+    |> getDirections y x
+    |> List.map (Array.toList >> viewingDistance height)
+    |> List.reduce (*)
+
 let solve1 filename =
     let grid = loadTreeGrid filename
 
@@ -29,4 +54,12 @@ let solve1 filename =
     |> Array.mapi (fun y row -> row |> Array.mapi (fun x _ -> isVisible y x grid))
     |> Array.sumBy (Array.filter id >> Array.length)
 
-let solve2 filename = -1
+let solve2 filename =
+    let grid = loadTreeGrid filename
+
+    grid
+    |> Array.mapi (fun y row ->
+        row
+        |> Array.mapi (fun x _ -> scenicScore y x grid)
+        |> Array.max)
+    |> Array.max
