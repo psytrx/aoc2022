1d9b3a146154ab46fdffecf43a53c1d8932deeb5
fdumont <fj.dumont@gmail.com>
2022-12-08 10:07:44 +0100

    Refactor day 8

--- problems/Day08.fs
+++ problems/Day08.fs
@@ -8,23 +8,19 @@ let loadTreeGrid =
 
 let hasObstruction height xs = Array.exists (fun h -> h >= height) xs
 
+let getDirections y x (grid: 'a array array) =
+    let top = grid.[.. y - 1] |> Array.map (fun row -> row.[x])
+    let bottom = grid.[y + 1 ..] |> Array.map (fun row -> row.[x])
+    let left = grid.[y].[.. x - 1]
+    let right = grid.[y].[x + 1 ..]
+    [ top; bottom; left; right ]
+
 let isVisible y x (grid: int array array) =
-    let height = grid.[y].[x] |> Util.dump "%i"
+    let height = grid.[y].[x]
 
-    let top =
-        grid.[.. y - 1]
-        |> Array.map (fun row -> row.[x])
-        |> hasObstruction height
-
-    let bottom =
-        grid.[y + 1 ..]
-        |> Array.map (fun row -> row.[x])
-        |> hasObstruction height
-
-    let left = grid.[y].[.. x - 1] |> hasObstruction height
-    let right = grid.[y].[x + 1 ..] |> hasObstruction height
-
-    not (top && bottom && left && right)
+    grid
+    |> getDirections y x
+    |> List.exists (hasObstruction height >> not)
 
 let solve1 filename =
     let grid = loadTreeGrid filename
