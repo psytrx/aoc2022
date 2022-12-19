69f3ce4338f68e4d4abec22fd00039d35394d1fd
fdumont <fj.dumont@gmail.com>
2022-12-19 10:57:19 +0100

    Refactor day 15

--- problems/Day15.fs
+++ problems/Day15.fs
@@ -23,27 +23,35 @@ let extent ranges =
     let hi = Array.map snd ranges |> Array.max
     lo, hi
 
+let isInRange (lo, hi) x = x >= lo && x <= hi
+
 let countCovered ranges =
     let (elo, ehi) = extent ranges
     let mutable c = 0
 
     for x in [ elo..ehi ] do
-        let covered = Array.exists (fun (lo, hi) -> x >= lo && x <= hi) ranges
+        let covered = Array.exists (fun range -> isInRange range x) ranges
         if covered then c <- c + 1
 
     c
 
+let beaconsInLine line sensorData =
+    sensorData
+    |> Array.filter (snd >> snd >> (=) line)
+    |> Array.map snd
+    |> Array.distinct
+    |> Array.length
+
+let findUncovered ranges =
+    let (lo, hi) = extent ranges
+
+    [ lo..hi ]
+    |> List.filter (fun x -> not (Array.exists (fun range -> isInRange range x) ranges))
+
 let solve1 filename =
-    let sensorData = loadData filename
-
     let line = 2_000_000
-
-    let beaconsInLine =
-        sensorData
-        |> Array.filter (snd >> snd >> (=) line)
-        |> Array.map snd
-        |> Array.distinct
-        |> Array.length
+    let sensorData = loadData filename
+    let beaconsInLine = beaconsInLine line sensorData
 
     let covered =
         sensorData
@@ -52,4 +60,20 @@ let solve1 filename =
 
     covered - beaconsInLine
 
-let solve2 filename = -1
+let solve2 filename =
+    let sensorData = loadData filename
+
+    let line =
+        [ 0..4_000_000 ]
+        |> List.map (fun line ->
+            printfn "%d" line
+
+            let ranges =
+                sensorData
+                |> Array.choose (sensorLineCoverage line)
+
+            let uncovered = findUncovered ranges
+            line, uncovered)
+        |> List.find (snd >> List.length >> (=) 1)
+
+    line
