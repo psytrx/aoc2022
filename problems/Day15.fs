3bb6f59a194f8f192cb72019df08d160d626d3cf
fdumont <fj.dumont@gmail.com>
2022-12-23 20:02:13 +0100

    Solve day 15, part 2

--- problems/Day15.fs
+++ problems/Day15.fs
@@ -1,79 +1,81 @@
 module Day15
 
 let loadData filename =
-    System.IO.File.ReadAllLines(filename)
-    |> Array.map (fun line ->
-        let sx, sy, bx, by =
-            Sscanf.sscanf "Sensor at x=%d, y=%d: closest beacon is at x=%d, y=%d" line
+    let data =
+        System.IO.File.ReadAllLines(filename)
+        |> Array.map (fun line ->
+            let sx, sy, bx, by =
+                Sscanf.sscanf "Sensor at x=%d, y=%d: closest beacon is at x=%d, y=%d" line
 
-        (sx, sy), (bx, by))
+            let r = abs (sx - bx) + abs (sy - by)
+            ((sx, sy), r), (bx, by))
 
-let sensorLineCoverage line ((sx, sy), (bx, by)) =
-    let md = abs (bx - sx) + abs (by - sy)
-    let dy = abs (line - sy)
+    let sensors = data |> Array.map fst |> Array.toList
+    let beacons = data |> Array.map snd |> Set.ofArray |> Set.toList
+    sensors, beacons
 
-    if dy > md then
-        None
-    else
-        let dx = md - dy
-        Some(sx - dx, sx + dx)
+let lineCoverage n sensors =
+    sensors
+    |> List.choose (fun ((sx, sy), r) ->
+        let dy = abs (sy - n)
 
-let extent ranges =
-    let lo = Array.map fst ranges |> Array.min
-    let hi = Array.map snd ranges |> Array.max
-    lo, hi
+        if dy > r then
+            None
+        else
+            let dx = r - dy
+            Some(sx - dx, sx + dx))
 
-let isInRange (lo, hi) x = x >= lo && x <= hi
+let compressRanges ranges =
+    let rec aux acc =
+        function
+        | [] -> acc
+        | hd :: tl ->
+            match acc with
+            | [] -> aux [ hd ] tl
+            | last :: rest ->
+                if fst hd <= snd last then
+                    let merged = (min (fst hd) (fst last), max (snd hd) (snd last))
+                    aux (merged :: rest) tl
+                else
+                    aux (hd :: acc) tl
 
-let countCovered ranges =
-    let (elo, ehi) = extent ranges
-    let mutable c = 0
+    List.rev (aux [] (List.sortBy fst ranges))
 
-    for x in [ elo..ehi ] do
-        let covered = Array.exists (fun range -> isInRange range x) ranges
-        if covered then c <- c + 1
+let countCoverage ranges =
+    ranges |> List.sumBy (fun (lo, hi) -> hi - lo + 1)
 
-    c
-
-let beaconsInLine line sensorData =
-    sensorData
-    |> Array.filter (snd >> snd >> (=) line)
-    |> Array.map snd
-    |> Array.distinct
-    |> Array.length
-
-let findUncovered ranges =
-    let (lo, hi) = extent ranges
-
-    [ lo..hi ]
-    |> List.filter (fun x -> not (Array.exists (fun range -> isInRange range x) ranges))
+let rangeExtent ranges =
+    let lo = ranges |> List.map fst |> List.min
+    let hi = ranges |> List.map snd |> List.max
+    (lo, hi)
 
 let solve1 filename =
+    let sensors, beacons = loadData filename
     let line = 2_000_000
-    let sensorData = loadData filename
-    let beaconsInLine = beaconsInLine line sensorData
+    let coverage = lineCoverage line sensors |> compressRanges
+    let coveredSum = countCoverage coverage
 
-    let covered =
-        sensorData
-        |> Array.choose (sensorLineCoverage line)
-        |> countCovered
+    let beaconsInRow =
+        beacons
+        |> List.filter (snd >> (=) line)
+        |> List.length
 
-    covered - beaconsInLine
+    coveredSum - beaconsInRow
 
 let solve2 filename =
-    let sensorData = loadData filename
+    let sensors, _ = loadData filename
 
-    let line =
-        [ 0..4_000_000 ]
-        |> List.map (fun line ->
-            printfn "%d" line
+    let (y, coverage) =
+        [| 1..4_000_000 |]
+        |> Array.Parallel.map (fun line ->
+            let coverage = lineCoverage line sensors |> compressRanges
+            let (lo, hi) = rangeExtent coverage
+            let coveredSum = countCoverage coverage
+            let width = hi - lo + 1
+            (width - coveredSum, (line, coverage)))
+        |> Array.find (fst >> (=) 1)
+        |> snd
+        |> Util.dump "%A"
 
-            let ranges =
-                sensorData
-                |> Array.choose (sensorLineCoverage line)
-
-            let uncovered = findUncovered ranges
-            line, uncovered)
-        |> List.find (snd >> List.length >> (=) 1)
-
-    line
+    let x = (List.head coverage |> snd) + 1
+    bigint x * bigint 4_000_000 + bigint y
